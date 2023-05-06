5420378a7a2306252c6d8f3888d17e0b82ac6e44
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-07 06:18:40 +0900

    とりあえずリクエストをハンドリングしてみる

--- server/src/client/Client.ts
+++ server/src/client/Client.ts
@@ -1,6 +1,7 @@
 import * as WebSocket from "ws";
 import { IClientState } from "./state/ClientState";
 import { ClientStateEntry } from "./state/ClientStateEntry";
+import { instantiatePacket } from "@yanap/trpg-common/PacketFunctions";
 
 /**
  * クライアントクラス
@@ -17,8 +18,12 @@ export class Client extends EventTarget {
     socket.on("close", () => {
       this.dispatchEvent(new Event("disconnected"));
     });
-    socket.on("message", (data: WebSocket.RawData) => {
-      // TODO: パケット解析処理
+    socket.on("message", (data: string | Buffer) => {
+      const buffer = Buffer.from(data);
+      const packetId = buffer.readUint8(0);
+      const packet = instantiatePacket(packetId);
+      packet.decode(buffer);
+      this.state.onHandlePacket(packet);
     });
   }
 }
--- server/src/client/state/ClientStateEntry.ts
+++ server/src/client/state/ClientStateEntry.ts
@@ -1,6 +1,7 @@
 import { IPacket } from "@yanap/trpg-common/Packet";
 import { EPacketId } from "@yanap/trpg-common/packets/PacketId";
 import { IClientState } from "./ClientState";
+import { PacketJoinRequest } from "@yanap/trpg-common/packets/PacketJoinRequest";
 
 /**
  * クライアントステート: 参加
@@ -11,5 +12,13 @@ export class ClientStateEntry implements IClientState {
    * @param packet パケット
    */
   onHandlePacket(packet: IPacket): void {
+    switch (packet.packetId) {
+      case EPacketId.PACKET_JOIN_REQUEST:
+        {
+          const p = packet as PacketJoinRequest;
+          console.info("Join User", p.userName);
+          break;  
+        }
+    }
   }
 }
