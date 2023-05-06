69afd06d31e3ae0d0b49d07a9bd7ea760cbb190e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-06 22:57:04 +0900

    とりあえずWebSocketを構築して接続しに行くようにした

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+const socket = new WebSocket("ws://localhost:3000");
+socket.onopen = (() => {
+  console.info("Server connected.");
+});
 </script>
 
 <template lang="pug">
