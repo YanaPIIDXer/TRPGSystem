8d58190157fa19458cbe7557d40b7835d14545ca
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-07 06:52:49 +0900

    パケット生成関数をexportするようにした

--- common/src/exports.ts
+++ common/src/exports.ts
@@ -1,5 +1,6 @@
 export { IPacket } from "./Packet";
 export { EPacketId } from "./packets/PacketId";
+export { instantiatePacket } from "./PacketFunctions";
 
 export { PacketJoinRequest } from "./packets/PacketJoinRequest";
 export { PacketJoinResponse } from "./packets/PacketJoinResponse";
