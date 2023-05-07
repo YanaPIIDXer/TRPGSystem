6d6817b24dde4c22a584af06cb14e796c3fb977d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-07 16:34:41 +0900

    パケットイベントの仕組み

--- client/src/client/Client.ts
+++ client/src/client/Client.ts
@@ -3,14 +3,47 @@ import type { EPacketId, IPacket } from "@yanap/trpg-common";
 import { instantiatePacket } from "@yanap/trpg-common";
 
 /**
+ * パケット受信イベント
+ */
+export class PacketEvent extends Event {
+  /**
+   * コンストラクタ
+   * @param _packetId パケットID
+   * @param buffer バッファ
+   */
+  constructor(private _packetId: EPacketId, private buffer: Uint8Array) {
+    super("packet");
+  }
+
+  /**
+   * パケットID
+   */
+  get packetId(): EPacketId { return this._packetId; }
+
+  /**
+   * パケットオブジェクト生成
+   * @returns パケット
+   */
+  makePacket<T extends IPacket>(): T {
+    const packet = instantiatePacket(this._packetId);
+    if (packet.packetId !== this._packetId) { throw new Error("PacketType Not Matched") };
+    packet.decode(this.buffer);
+    return packet as T;
+  }
+}
+
+type PacketEventListener = (event: PacketEvent) => void;
+
+/**
  * クライアントクラス
  */
-export class Client {
+export class Client extends EventTarget {
   /**
    * コンストラクタ
    * @param socket WebSocketインスタンス
    */
   constructor(private socket: WebSocket) {
+    super();
     this.socket.onmessage = this.onRecv.bind(this);
   }
 
@@ -30,9 +63,23 @@ export class Client {
     });
     const decodedBuffer = decode(buffer) as Uint8Array;
     const packetId = decodedBuffer[0] as EPacketId;
-    console.info("Recv Packet", `ID: ${packetId}`);
-    const packet = instantiatePacket(packetId);
-    packet.decode(buffer);
+    this.dispatchEvent(new PacketEvent(packetId, buffer));
+  }
+
+  /**
+   * パケットイベントリスナ追加
+   * @param listener イベントリスナ
+   */
+  addPacketEventListener(listener: PacketEventListener) {
+    this.addEventListener("packet", listener as EventListener);
+  }
+
+  /**
+   * パケットイベントリスナ除去
+   * @param listener イベントリスナ
+   */
+  removePacketEventListener(listener: PacketEventListener) {
+    this.removeEventListener("packet", listener as EventListener);
   }
 
   /**
--- client/src/routes/views/JoinView.vue
+++ client/src/routes/views/JoinView.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
-import { ref, inject } from "vue";
-import type { Client } from "../../client/Client";
+import { EPacketId, PacketJoinRequest } from "@yanap/trpg-common";
+import { ref, inject, onUnmounted } from "vue";
+import type { Client, PacketEvent } from "../../client/Client";
 
 const client = inject<Client>("client")!;
 const userName = ref("");
+
+const onRecvPacket = (e: PacketEvent) => {
+  switch (e.packetId) {
+    case EPacketId.PACKET_JOIN_RESPONSE:
+      // TODO: 画面遷移
+      alert(userName.value);
+      break;
+  }
+};
+
+client.addPacketEventListener(onRecvPacket);
 
 /**
  * 参加
  */
 const onJoin = () => {
-  alert(userName.value);
-}
+  client.sendPacket(new PacketJoinRequest(userName.value));
+};
+
+onUnmounted(() => {
+  client.removePacketEventListener(onRecvPacket);
+});
 
 defineExpose({
   userName,
@@ -24,7 +40,7 @@ defineExpose({
   form(@submit.prevent="onJoin")
     input(type="text" v-model="userName" placeholder="ユーザ名を入力してください")
     br
-    input(type="submit" value="参加")
+    input(type="submit" value="参加" :disabled="!userName")
 </template>
 
 <style lang="sass" scoped>
