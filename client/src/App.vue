ddf63993c0ca39743190c84545e300031fba97fe
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-07 15:56:09 +0900

    送受信処理準備

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,10 @@
 <script setup lang="ts">
 import { inject } from "vue";
-import { PacketJoinRequest } from "@yanap/trpg-common";
 import { Client } from "./client/Client";
-import { decode } from '@msgpack/msgpack';
-import type { EPacketId } from "@yanap/trpg-common";
-import { instantiatePacket } from "@yanap/trpg-common";
 
 const socket = new WebSocket("ws://localhost:3000");
 const client = new Client(socket);
 inject("client", client);
-
-/*
-socket.onopen = (() => {
-  const packet = new PacketJoinRequest("キャベツ 太郎");
-  const buffer = packet.encode();
-  socket.send(buffer);
-});
-
-socket.onmessage = async (e: MessageEvent<Blob>) => {
-  const buffer = await new Promise<Uint8Array>(resolve => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      resolve(new Uint8Array(reader.result as ArrayBuffer));
-    }
-    reader.readAsArrayBuffer(e.data);
-  });
-  const decodedBuffer = decode(buffer) as Uint8Array;
-  const packetId = decodedBuffer[0] as EPacketId;
-  console.info("Recv Packet", `ID: ${packetId}`);
-  const packet = instantiatePacket(packetId);
-  packet.decode(buffer);
-};
-*/
 
 </script>
 
--- client/src/client/Client.ts
+++ client/src/client/Client.ts
@@ -1,3 +1,7 @@
+import { decode } from '@msgpack/msgpack';
+import type { EPacketId, IPacket } from "@yanap/trpg-common";
+import { instantiatePacket } from "@yanap/trpg-common";
+
 /**
  * クライアントクラス
  */
@@ -7,5 +11,36 @@ export class Client {
    * @param socket WebSocketインスタンス
    */
   constructor(private socket: WebSocket) {
+    this.socket.onmessage = this.onRecv.bind(this);
+  }
+
+  /**
+   * 受信イベント
+   * @param e イベント
+   */
+  async onRecv(e: MessageEvent): Promise<void> {
+    const buffer = await new Promise<Uint8Array>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        resolve(new Uint8Array(reader.result as ArrayBuffer));
+      }
+      reader.onerror = reject;
+      reader.onabort = reject;
+      reader.readAsArrayBuffer(e.data);
+    });
+    const decodedBuffer = decode(buffer) as Uint8Array;
+    const packetId = decodedBuffer[0] as EPacketId;
+    console.info("Recv Packet", `ID: ${packetId}`);
+    const packet = instantiatePacket(packetId);
+    packet.decode(buffer);
+  }
+
+  /**
+   * パケット送信
+   * @param packet パケット
+   */
+  sendPacket(packet: IPacket): void {
+    const buffer = packet.encode();
+    this.socket.send(buffer);
   }
 }
