6f49630b3fc119e386cb0518bb97f2070a6742cb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-07 12:05:54 +0900

    エラーは出るが一旦コミット

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { PacketJoinRequest } from "@yanap/trpg-common";
+
 const socket = new WebSocket("ws://localhost:3000");
 socket.onopen = (() => {
   console.info("Server connected.");
